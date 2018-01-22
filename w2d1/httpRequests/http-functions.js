8bc59fd41d08c4402b31f9a72af9e081f07aeef0
XiaofengLighthouseLabs <gaga0110@163.com>
2018-01-22 22:00:42 +0000

    buffer print at the end of the module

--- w2d1/httpRequests/http-functions.js
+++ w2d1/httpRequests/http-functions.js
@@ -1,19 +1,20 @@
 
 var https = require ('https');
-module.exports = function getHTML (options, callback) {
+module.exports = function (options, callback) {
 
     https.get(options, function(response){
       response.setEncoding('utf8');
       var buffer = '';
-      response.on('data', function(data){
-        if(data){
-            buffer = buffer + data;
+      response.on('data', function(chunk){
+        if(chunk){
+            buffer = buffer + chunk;
         }
-        console.log('Chunk Received.Length:', data.length);
-        callback (buffer);
+        console.log('Chunk Received.Length:', chunk.length);
+
       });
       response.on('end', function() {
         console.log('Response stream complete.');
+        callback (buffer);
       });
     });
 
--- w2d1/httpRequests/print_lowercase.js
+++ w2d1/httpRequests/print_lowercase.js
@@ -2,7 +2,7 @@ var getHTML = require('./http-functions');
 
 var requestOptions = {
   host: 'sytantris.github.io',
-  path: '/http-examples/step6.html'
+  path: '/http-examples/step6/lowercase.html'
 };
 getHTML(requestOptions,function(html) {
   console.log(html.toLowerCase());
